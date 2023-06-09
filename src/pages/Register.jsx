fb32cc749c19dea40ccef9326749808db922d80e
yalegama <nadunyalegama@gmail.com>
2023-06-09 20:26:18 +0530

    Created Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Login from './Login'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className="formContainer">
+        <div className="formWrapper">
+            <form action="">
+                <input type="text" placeholder='Display Name' />
+                <input type="email" placeholder='Email' />
+                <input type="password" placeholder='Password' />
+                <input type="file" />
+                <button>Sign Up</button>
+            </form>
+            <p>Do you have an account? <a href={<Login/>}>Login</a> here </p>
+        </div>
+    </div>
   )
 }
 
