8fb84b32398da0d0177ae992b91d2478461da27d
yalegama <nadunyalegama@gmail.com>
2023-06-14 00:59:48 +0530

    Created SideBar Page and Chat Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Register from './Register'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="formContainer">
+        <div className="formWrapper">
+            <span className="logo">Chat Book</span>
+            <span className="title">Login</span>
+            <form className='form' action="">
+                <input type="email"  placeholder='Email'/>
+                <input type="password"  placeholder='Password'/>
+                <button>Sign Up</button>
+            </form>
+            <p>You don't have an account? <a href={<Register/>}>Register</a> here</p>
+        </div>
+    </div>
   )
 }
 
